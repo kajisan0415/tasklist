cc10ae0fe133ae08c6d5e96d039bc7df7bf1abcf
kajisan0415 <agonagakajisan0415@gmail.com>
2021-08-05 13:30:19 +0000

    投稿機能

--- app/controllers/seminars_controller.rb
+++ app/controllers/seminars_controller.rb
@@ -2,4 +2,26 @@ class SeminarsController < ApplicationController
   def index
     @seminars = Seminar.all
   end
+  
+  def new
+    @seminar = Seminar.new
+  end
+  
+  def create
+    @seminar = Seminar.new(seminar_params)
+    #@article.user_id = current_user.id
+    if @seminar.save
+      redirect_to seminar_path(@seminar), notice: "投稿しました"
+    else
+      render(:new) && return
+    end
+  end
+  
+  def show
+    @seminar = Seminar.find(params[:id])
+  end
+  
+  private seminar_params
+    params.require(:seminar).permit(:title, :starts_at, :ends_at)
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   get 'seminars/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :tasks
-  resources :seminars, only: [:index]
+  resources :seminars
 end
