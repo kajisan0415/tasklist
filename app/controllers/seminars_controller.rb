9323937f9c4d5b6479d7a845869c6d526460bfe2
kajisan0415 <agonagakajisan0415@gmail.com>
2021-08-06 10:15:40 +0000

    投稿機能

--- app/controllers/seminars_controller.rb
+++ app/controllers/seminars_controller.rb
@@ -2,11 +2,11 @@ class SeminarsController < ApplicationController
   def index
     @seminars = Seminar.all
   end
-  
+
   def new
     @seminar = Seminar.new
   end
-  
+
   def create
     @seminar = Seminar.new(seminar_params)
     #@article.user_id = current_user.id
@@ -16,12 +16,13 @@ class SeminarsController < ApplicationController
       render(:new) && return
     end
   end
-  
+
   def show
     @seminar = Seminar.find(params[:id])
   end
-  
-  private seminar_params
-    params.require(:seminar).permit(:title, :starts_at, :ends_at)
 
+  private 
+  def seminar_params
+    params.require(:seminar).permit(:title, :starts_at, :ends_at)
+  end
 end
